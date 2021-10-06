77d273277a20427c385a0dad5a5a2807de15c1d2
KouOsada <kouosada.0930@gmail.com>
2021-10-06 11:07:56 +0000

    [Add]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :index, :edit, :update]
 
-  resources :books do
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
 
 end
